095913862eb11cebf4f0a23c886fd39d7919643c
Angi-Adema <dr.adema@gmail.com>
2026-06-19 11:23:04 -0600

    added readTextFile() method to Main.java.

--- src/linkedlistADT/Main.java
+++ src/linkedlistADT/Main.java
@@ -1,8 +1,33 @@
 package linkedlistADT;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 
 public class Main {
+	
+    // Create method to read in text file and call CustomLinkedList to process data output
+    public void readTextFile(String fileName, CustomLinkedList linkedList) {
+    	
+    	// Try with resources to close resources automatically and error handling for file access
+    	// Use BufferedReader to read in the text file data
+    	try (BufferedReader br = new BufferedReader(new FileReader("file.txt"))) {
+    		
+    		// Store read in text data
+    		String text;
+    		
+    		// Check to see if there is still text to be read in and covert it to int from String
+    		while((text = br.readLine()) != null) {
+    			linkedList.insert(Integer.parseInt(text));
+    		}
+    	} catch (IOException e) {
+    		
+    		// Print error messages
+    		System.out.println("Error reading the file, please try again!" + e.getMessage());
+    	}
+    }
+
 
 	public static void main(String[] args) {
         CustomLinkedList linkedList = new CustomLinkedList();
@@ -17,5 +42,6 @@ public class Main {
         while (iterator.hasNext()) {
             System.out.print(iterator.next() + " ");
         }
-    }
+        
+	}
 }
